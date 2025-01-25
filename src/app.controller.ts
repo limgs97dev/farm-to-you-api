1c462d0cf25a36d7f9ded90e9cc4c52e6da0ac18
limgs <limgs97dev@gmail.com>
2025-01-26 00:43:07 +0900

    [FTY-2] feat: add k8s logic

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER, WinstonLogger } from 'nest-winston';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
+  constructor(
+    private readonly appService: AppService,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
   @Get()
   getHello(): string {
+    this.logger.info('test!');
     return this.appService.getHello();
   }
 }
